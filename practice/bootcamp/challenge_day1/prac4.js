7096e2b705c59b1edbe71edc12f7b77681da2144
Manoj Kumar <125752851+Manoj-p2304@users.noreply.github.com>
2023-10-19 18:54:26 +0530

    Deleted unnecessary files

--- practice/bootcamp/challenge_day1/prac4.js
+++
@@ -1,9 +0,0 @@
-function timeCon(seconds){
-    
-    let minute = seconds / 60;
-    let hours = minute / 60;
-    console.log(`${seconds} seconds is equal to ${hours} hours,  ${minute} minutes and ${seconds} seconds`)
-}
-
-let seconds = 7515 ;
-console.log(timeCon(seconds));
